cd4444d2c95dfa4d53f36f7f0eb65e47c8509892
jhaeme15 <jaredhaeme15@augustana.edu>
2019-03-31 15:05:40 -0500

    added firebase

--- app/src/main/java/com/example/pushnotifiction/MainActivity.java
+++ app/src/main/java/com/example/pushnotifiction/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.pushnotifiction;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Main Activity";
+    private DatabaseReference database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Write a message to the database
+        database = FirebaseDatabase.getInstance().getReference();
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                    }
+                });
+    }
+
+    public void storeTokenFirebase(String token){
+        database.push().setValue(token);
     }
 
 }
